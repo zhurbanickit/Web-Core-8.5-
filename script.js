86dbe5ecd5ea5af1dc76582bc4549b5d7dfc24dc
zhurbanickit <zhurbaa0@gmail.com>
2025-10-15 21:41:46 +0900

    perfect commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelector(".hide").onclick = function () {
+  console.log("click up");
+  document.querySelector(".show").style.display = "flex";
+  document.querySelector(".hide").style.display = "none";
+  document.querySelector(".hidden_buttons").style.display = "none";
+};
+
+document.querySelector(".show").onclick = function () {
+  console.log("click show");
+  document.querySelector(".show").style.display = "none";
+  document.querySelector(".hide").style.display = "flex";
+  document.querySelector(".hidden_buttons").style.display = "flex";
+};
